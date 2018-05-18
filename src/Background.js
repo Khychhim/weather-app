7a910f980eeced4fd4fb70df1ad1cd82ee5d5ed3
Lim <khychhim_lim@dc01.fujixerox.net>
2018-05-18 15:51:31 +1200

    API Class

---
+++ src/Background.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Background extends React.Component{
+  constructor(){
+    super();
+
+  }
+
+  componentDidMount(){
+
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Background;
